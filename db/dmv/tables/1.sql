1f743716a10c61506f8cf4770d076657fea35b26
swatips1 <43992589+swatips1@users.noreply.github.com>
2024-05-01 08:36:42 -0400

    Update 1.sql

--- db/dmv/tables/1.sql
+++ db/dmv/tables/1.sql
@@ -1,3 +1,5 @@
+DO $$ BEGIN RAISE NOTICE 'Creating table users at %',now(); END; $$;
+
 CREATE TABLE users (id SERIAL, name VARCHAR(60), pass TEXT);
 ALTER TABLE users ADD CONSTRAINT PK_users PRIMARY KEY (id);
 ALTER TABLE users ADD CONSTRAINT UQ_users UNIQUE (pass);
