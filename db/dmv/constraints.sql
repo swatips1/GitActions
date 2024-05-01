fce1bdb81ec2ebf4b0935b12580c2380461297da
swatips1 <43992589+swatips1@users.noreply.github.com>
2024-05-01 08:39:16 -0400

    Update constraints.sql

--- db/dmv/constraints.sql
+++ db/dmv/constraints.sql
@@ -1,1 +1,5 @@
+DO $$ BEGIN RAISE NOTICE 'Start of constraint creation at %',now(); END; $$;
+
 ALTER TABLE teacher ADD CONSTRAINT FK_teacher_person FOREIGN KEY (person_id) REFERENCES person(id);
+
+DO $$ BEGIN RAISE NOTICE 'End of constraint creation at %',now(); END; $$;
